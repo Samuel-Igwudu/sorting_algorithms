fb207dd5fbaa33429879b793708e689021f0f1db
Samuel-Igwudu <igwudu03@gmail.com>
2022-10-10 14:37:48 -0700

    modified

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,7 +17,7 @@ void bubble_sort(int *array, size_t size)
 		swap = 0;
 		for (i = 0; (i + 1) < n; i++)
 		{
-			if (array[i] > array[i + ])
+			if (array[i] > array[i + 1])
 			{
 				tmp = array[i + 1];
 				array[i + 1] = array[i];
